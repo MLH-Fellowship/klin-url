45fc4f62d784cb0d05a666bda73a68be6b575bb6
k07-repo <kpuli007@fiu.edu>
2020-11-24 22:32:29 -0500

    Add quick url validation function

--- klinurl-addon/chrome/js/code/background_apirequest.js
+++ klinurl-addon/chrome/js/code/background_apirequest.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener(function
 
     // changeInfo.url includes the new url
     // check to make sure it includes http so it doesn't come up in the new tab page
-    if (changeInfo.url && changeInfo.url.includes('http')) {
+    if (changeInfo.url && is_url(str)) {
         alert(changeInfo.url) //DEBUG: Makes popup with new url every time it loads
 
         // https://attacomsian.com/blog/xhr-json-post-request
@@ -36,3 +36,16 @@ chrome.tabs.onUpdated.addListener(function
         xhr.send(JSON.stringify(json));
     }
 });
+
+function is_url(str) {
+    
+    // https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+    if (regexp.test(str)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}

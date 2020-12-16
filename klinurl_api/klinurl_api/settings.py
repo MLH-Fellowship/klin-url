254a23e0d382d4639172781023f0fb671ccf901d
Nyior Clement Jr <cnyior27@gmail.com>
2020-12-16 22:29:29 +0100

    cors allow all

--- klinurl_api/klinurl_api/settings.py
+++ klinurl_api/klinurl_api/settings.py
@@ -155,8 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-CORS_ALLOWED_ORIGINS = [
-    "http://www.klinurl.me",
-    "http://localhost:8000",
-]
+CORS_ALLOW_ALL_ORIGINS=True
 
